17311f73b2d7aec6bd74a7b0fd8899b21b193f16
jarulraj <jarulraj@cs.cmu.edu>
2018-09-26 16:10:36 -0400

    Added NL join algorithm

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -33,26 +33,56 @@ int generate_number(){
 
 void RunJoinBenchmark(){
 
-	std::vector<int> column_1;
-	std::vector<int> column_2;
+	int* column_1;
+	int* column_2;
 	int column_1_size = 50;
-	int column_2_size = column_1_size/10;
+	int column_2_size = column_1_size/5;
+
+	// Initialize arrays
+	column_1 = new int[column_1_size];
+	column_2 = new int[column_2_size];
 
 	// Load data
 	for(int column_1_itr = 0; column_1_itr < column_1_size; column_1_itr++){
 		auto number = generate_number();
-		column_1.push_back(number);
-		std::cout << number << " ";
+		column_1[column_1_itr] = number;
+		std::cout << column_1[column_1_itr] << " ";
 	}
 	std::cout << "\n";
 
 	for(int column_2_itr = 0; column_2_itr < column_2_size; column_2_itr++){
 		auto number = generate_number();
-		column_2.push_back(number);
-		std::cout << number << " ";
+		column_2[column_2_itr] = number;
+		std::cout << column_2[column_2_itr] << " ";
 	}
 	std::cout << "\n";
 
+	std::vector<std::pair<int,int>> matches;
+
+	// NESTED LOOP JOIN
+	for(int column_1_itr = 0; column_1_itr < column_1_size; column_1_itr++){
+		auto column_1_number = column_1[column_1_itr];
+
+		for(int column_2_itr = 0; column_2_itr < column_2_size; column_2_itr++){
+			auto column_2_number = column_2[column_2_itr];
+
+			// Check if numbers match
+			if(column_1_number == column_2_number){
+				// Add to match list
+				matches.push_back(std::make_pair(column_1_itr, column_2_itr));
+			}
+		}
+	}
+
+	std::cout << "MATCHES: \n";
+	for(auto match: matches){
+		std::cout << column_1[match.first] << " :: " << match.first << " " << match.second << "\n";
+	}
+	std::cout << "\n";
+
+	// Clean up arrays
+	delete column_1;
+	delete column_2;
 
 }
 
