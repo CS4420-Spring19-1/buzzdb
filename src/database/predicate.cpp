61d3932804810a0841b07babf3cc603cf9292bdc
jonleeyz <jonleeyz@outlook.com>
2019-03-23 09:42:43 -0400

    [predicate.cpp] Reorganise code chunks; add spacing and comments

--- src/database/predicate.cpp
+++ src/database/predicate.cpp
@@ -1,38 +1,43 @@
 #include "predicate.h"
 
-namespace emerald
-{
-    Predicate::opType toOpType(std::string op){
-        if(op.compare("=")==0){
-            return Predicate::opType::EQ;
-        } else if(op.compare("!=")==0){
-            return Predicate::opType::NE;
-        } else if(op.compare(">")==0){
-            return Predicate::opType::GT;
-        } else if(op.compare("<")==0){
-            return Predicate::opType::LT;
-        } else if(op.compare(">=")==0){
-            return Predicate::opType::GE;
-        } else {
-            return Predicate::opType::LE;
-        }
-    };
-
-    Predicate::Predicate(std::string column_name, std::string op, std::string value){
-        this->column = column_name;
+namespace emerald {
+    
+    Predicate::Predicate(std::string column_name, std::string op, std::string value) {
+        this->column = column_name; // interface could be wrong here: no pointers in interface
         this->op = toOpType(op);
         this->value = value;
     };
 
-    std::string Predicate::getColumn(){
+    std::string Predicate::getColumn() {
         return this->column;
     };
 
-    std::string Predicate::getValue(){
+    std::string Predicate::getValue() {
         return this->value;
     };
 
-    Predicate::opType Predicate::getOp(){
+    Predicate::opType Predicate::getOp() {
         return this->op;
+    };
+
+    /** 
+     * 1. Why use compare for strings?
+     * 2. Should this function's signature be declared in predicate.h?
+     * 3. Is it opstring being a valid op a prerequisite?
+     */
+    Predicate::opType toOpType(std::string op) {
+        if (op.compare("=") == 0) {
+            return Predicate::opType::EQ;
+        } else if (op.compare("!=") == 0) {
+            return Predicate::opType::NE;
+        } else if (op.compare(">") == 0) {
+            return Predicate::opType::GT;
+        } else if (op.compare("<") == 0) {
+            return Predicate::opType::LT;
+        } else if (op.compare(">=") == 0) {
+            return Predicate::opType::GE;
+        } else {
+            return Predicate::opType::LE;
+        }
     };
 } // emerald
