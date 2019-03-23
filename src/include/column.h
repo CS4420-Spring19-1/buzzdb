5e85ac6a0dba282f25b3b3e606bf239a08225859
jonleeyz <jonleeyz@outlook.com>
2019-03-23 14:43:18 -0400

    [column.h] Add spacing and comments

--- src/include/column.h
+++ src/include/column.h
@@ -2,18 +2,22 @@
 #include "field.h"
 #include <vector>
 
-namespace emerald
-{
-    class Column
-    {
+namespace emerald {
+    class Column {
         private:
             std::vector<Field*> fields_;
+
         public:
             Column(/* args */);
+
             ~ Column();
+
             size_t size() const;
+
             void insert(Field* field);
+
             Field* get_field(int index) const;
+
             std::vector<Field*> get_fields() const;
     };
 } // emerald
