273a23877614fdd6f8431613c1e5c9804f7dec74
jonleeyz <jonleeyz@outlook.com>
2019-03-23 10:18:07 -0400

    [integer_field.cpp] Add spacing and comments

--- src/database/integer_field.cpp
+++ src/database/integer_field.cpp
@@ -3,32 +3,34 @@
 
 namespace emerald
 {
-    IntegerField::IntegerField(int v):Field(field_type::INTEGER),value(v){};
+    IntegerField::IntegerField(int v) : Field(field_type::INTEGER), value(v) {
+    };
 
-    void IntegerField::print() const{
+    void IntegerField::print() const {
         std::cout << value << " ";
     };
 
-    bool IntegerField::filter(Predicate::opType op, Field* value){
+    bool IntegerField::filter(Predicate::opType op, Field* value) {
         IntegerField* int_value = static_cast<IntegerField*>(value);
-        switch (op)
-        {
-            case Predicate::opType::EQ :
-                return this->value==int_value->getValue();
+        switch (op) {
+            // SEG FAULT
+            // value is a variable, not a pointer; should not be dereferenced
+            case Predicate::opType::EQ:
+                return this->value == int_value->getValue();
                 break;
-            case Predicate::opType::NE :
-                return this->value!= int_value->getValue();
+            case Predicate::opType::NE:
+                return this->value != int_value->getValue();
                 break;
-            case Predicate::opType::GT :
+            case Predicate::opType::GT:
                 return this->value > int_value->getValue();
                 break;
-            case Predicate::opType::LT :
+            case Predicate::opType::LT:
                 return this->value < int_value->getValue();
                 break;
-            case Predicate::opType::GE :
+            case Predicate::opType::GE:
                 return this->value >= int_value->getValue();
                 break;
-            case Predicate::opType::LE :
+            case Predicate::opType::LE:
                 return this->value <= int_value->getValue();
                 break;
             default:
@@ -37,12 +39,15 @@ namespace emerald
         } 
     };
 
-    int IntegerField::getValue() const{
+    int IntegerField::getValue() const {
+        // SEG FAULT
+        // value is a variable, not a pointer; should not be dereferenced
         return this->value;
     };
 
     /*copy constructor for integer field*/
-    IntegerField::IntegerField(const IntegerField& field):Field(field_type::INTEGER){
+    // exceeds 80 spaces
+    IntegerField::IntegerField(const IntegerField& field) : Field(field_type::INTEGER) {
         value = field.getValue();
     };
 } // emerald
