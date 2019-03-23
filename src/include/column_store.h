c66ded528fc2d18d95661a4404066734a9a95ddc
jonleeyz <jonleeyz@outlook.com>
2019-03-23 13:43:31 -0400

    [column_store.h] Add spacing and comments

--- src/include/column_store.h
+++ src/include/column_store.h
@@ -7,12 +7,22 @@ namespace emerald
     class ColumnStore : public Table {
         private:
             std::vector<Column*> columns_; 
+
         public:
             ColumnStore(int table_id);
+
+            // destructor not declared or implemented; base implementation used
+
+            // possible to add override keyword
             void print() const;
+
+            // possible to add override keyword
             size_t size() const;
+
             void insert_tuple(std::vector<Field*> fields);
+
             std::vector<Column*> get_columns() const;
+
             Column* get_column(int index) const;
     };
 } // emerald
