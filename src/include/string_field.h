0bd76918c6b3694be9e814f30096e79707ac0396
jonleeyz <jonleeyz@outlook.com>
2019-03-23 10:02:30 -0400

    [string_field.h] Add spacing

--- src/include/string_field.h
+++ src/include/string_field.h
@@ -3,16 +3,20 @@
 #include "field.h"
 #include <string>
 
-namespace emerald
-{
-    class StringField : public Field{
+namespace emerald {
+    class StringField : public Field {
         private:
             std::string value;
+
         public:
             StringField(std::string v);
+            
             void print() const;
+
             bool filter(Predicate::opType op, Field* value);
+
             std::string getValue() const;
+
             StringField(const StringField& field);
     };
 } // emerald
