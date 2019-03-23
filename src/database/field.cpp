6bda4e26a488d88ac49ad7b9428af374c197b710
jonleeyz <jonleeyz@outlook.com>
2019-03-23 10:04:40 -0400

    [field.cpp] Add spacing

--- src/database/field.cpp
+++ src/database/field.cpp
@@ -1,7 +1,6 @@
 #include "field.h"
 #include "field_type.h"
 
-namespace emerald
-{
-    Field::Field(field_type type):type(type){};
+namespace emerald {
+    Field::Field(field_type type) : type(type) {};
 } // emerald
