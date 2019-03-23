ec07654e68dd5f3b3cb992d5dac5a0c22a44ae54
jonleeyz <jonleeyz@outlook.com>
2019-03-23 10:02:22 -0400

    [integer_field.h] Add spacing

--- src/include/integer_field.h
+++ src/include/integer_field.h
@@ -2,16 +2,20 @@
 
 #include "field.h"
 
-namespace emerald
-{
-   class IntegerField : public Field{
+namespace emerald {
+   class IntegerField : public Field {
        private:
             int value;
+
         public:
             IntegerField(int v);
+
             void print() const;
+
             bool filter(Predicate::opType op, Field* value);
+
             int getValue() const;
+
             IntegerField(const IntegerField& field);
    }; 
 } // emerald
