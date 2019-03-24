e370d7c6d0e9ac254552216371ff70fc55ff1b3e
jonleeyz <jonleeyz@outlook.com>
2019-03-23 20:30:02 -0400

    [summary_list.h] Add spacing

--- src/include/summary_list.h
+++ src/include/summary_list.h
@@ -3,16 +3,19 @@
 #include "summary.h"
 #include "tuple_set.h"
 
-namespace emerald
-{
+namespace emerald {
     class SummaryList : public Summary {
         private:
             std::vector<TupleSet*> tuple_set_list_;
+
         public:
             SummaryList();
-            void add_tuple_set(TupleSet* tuple_set);
-            std::vector<TupleSet*> get_tuples() const;
-            size_t size() const;
 
+            void add_tuple_set(TupleSet* tuple_set);
+
+            std::vector<TupleSet*> get_tuples() const;
+
+            // can add override keyword here
+            size_t size() const;
     };
 } // emerald
