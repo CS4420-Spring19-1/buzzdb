232cfb1986f34f1612d35c4011bcace5e30d7b34
jonleeyz <jonleeyz@outlook.com>
2019-03-25 12:30:30 -0400

    [double_field.h] Refactor: Reorganize code chunks; add interface comments; update whitespace and indentation

--- src/include/double_field.h
+++ src/include/double_field.h
@@ -2,16 +2,38 @@
 
 #include "field.h"
 
-namespace emerald
-{
-    class DoubleField : public Field{
-        private:
-            double value;
-        public:
-            DoubleField(double v);
-            void print() const;
-            bool filter(Predicate::opType op, Field* value);
-            double getValue() const;
-            DoubleField(const DoubleField& field);
-    };
-} // emerald
+namespace emerald {
+class DoubleField : public Field {
+private:
+  double value;
+
+  // not implemented: equality operator, serialize, getType
+
+public:
+  /**
+   * Constructor for the DoubleField class
+   */
+  DoubleField(double v);
+
+  /**
+   * Copy constructor for the DoubleField class
+   */
+  DoubleField(const DoubleField &field);
+
+  /**
+   * Prints the double field's contents
+   */
+  void print() const override;
+
+  /**
+   * Compares the given field with this field using the op specified in the predicate.
+   * Equivalent to field.compare() in the java implementation.
+   */
+  bool filter(Predicate::opType op, Field *value) override;
+
+  /**
+   * Gets the double field's value
+   */
+  double getValue() const;
+};
+}
