5be22a68c06c16f365610b2a4667b88d749fe808
jonleeyz <jonleeyz@outlook.com>
2019-03-25 12:27:52 -0400

    [string_field.h] Refactor: Reorganize code chunks; add interface comments; update whitespace and indentation

--- src/include/string_field.h
+++ src/include/string_field.h
@@ -3,16 +3,38 @@
 #include "field.h"
 #include <string>
 
-namespace emerald
-{
-    class StringField : public Field{
-        private:
-            std::string value;
-        public:
-            StringField(std::string v);
-            void print() const;
-            bool filter(Predicate::opType op, Field* value);
-            std::string getValue() const;
-            StringField(const StringField& field);
-    };
-} // emerald
+namespace emerald {
+class StringField : public Field {
+private:
+  std::string value;
+
+  // not implemented: equality operator, serialize, getType
+
+public:
+  /**
+   * Constructor for the StringField class
+   */
+  StringField(std::string v);
+
+  /**
+   * Copy constructor for the StringField class
+   */
+  StringField(const StringField &field);
+
+  /**
+   * Prints the string field's contents
+   */
+  void print() const override;
+
+  /**
+   * Compares the given field with this field using the op specified in the predicate.
+   * Equivalent to field.compare() in the java implementation.
+   */
+  bool filter(Predicate::opType op, Field *value) override;
+
+  /**
+   * Gets the string field's value
+   */
+  std::string getValue() const;
+};
+}
