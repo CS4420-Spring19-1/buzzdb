21f710c6104b6c2db8d1229039879c3bc23ef676
jonleeyz <jonleeyz@outlook.com>
2019-03-25 11:47:23 -0400

    [tuple.h] Refactor: add interface comments; update whitespace and indentation

--- src/include/tuple.h
+++ src/include/tuple.h
@@ -4,18 +4,57 @@
 #include "field.h"
 #include "tuple_descriptor.h"
 
-namespace emerald{
-    class Tuple{
-        private:
-            std::vector<Field*> fields_;
-        public:
-            Tuple();
-            Tuple(std::vector<Field*> fields);
-            void print() const;
-            Field* getField(int index) const;
-            Tuple(const Tuple& tuple);
-            std::vector<Field*> get_fields() const;
-            void append_fields(std::vector<Field*> fields);
-            void add_field(Field* field);
-    };
+namespace emerald {
+class Tuple {
+ private:
+  /**
+   * Internal representation of a tuple
+   */
+  std::vector<Field*> fields_;
+
+ public:
+  /**
+   * No argument constructor of the Tuple class
+   */
+  Tuple();
+
+  /**
+   * Constructor of the Tuple class
+   */
+  Tuple(std::vector<Field*> fields);
+
+  /**
+   * Copy constructor of the Tuple class
+   */
+  Tuple(const Tuple& tuple);
+
+  /**
+   * Prints the tuple's contents
+   */
+  void print() const;
+  /**
+   * Returns a pointer to the ith field in the tuple.
+   * Returns null if field is not set.
+   */
+  Field* getField(int index) const;
+
+
+  /**
+   * Returns a vector of all fields in the tuple.
+   * Returns null if field is not set (?).
+   */
+  std::vector<Field*> get_fields() const;
+
+  /**
+   * Returns a pointer to the ith field in the tuple.
+   * Returns null if field is not set.
+   */
+  void append_fields(std::vector<Field*> fields);
+
+  /**
+   * Returns a pointer to the ith field in the tuple.
+   * Returns null if field is not set.
+   */
+  void add_field(Field* field);
 };
+}
