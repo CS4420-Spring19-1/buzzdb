8077915d1af941fb6475108763707b8154565bd0
jonleeyz <jonleeyz@outlook.com>
2019-03-25 12:27:35 -0400

    [field.h] Refactor: Reorganize code chunks; add interface comments; update whitespace and indentation

--- src/include/field.h
+++ src/include/field.h
@@ -3,20 +3,34 @@
 #include "field_type.h"
 #include "predicate.h"
 
-namespace emerald
-{
-  class Field
-  {
-    private:
-      field_type type;
-    public:
-      Field();
-      Field(field_type type);
-      virtual void print() const=0;
-      // if filter returns true, the tuple/attribute value needs to be included in the result
-      virtual bool filter(Predicate::opType op, Field* value)=0;
-  };
-  
-  
-    
-}; // emerald
+namespace emerald {
+class Field {
+ private:
+  field_type type;
+
+  // not implemented: equality operator, serialize, getType
+
+ public:
+  /**
+   * No argument constructor for the Field class
+   */
+  Field();
+
+  /**
+   * Constructor for the Field class
+   */
+  Field(field_type type);
+
+  /**
+   * Prints the field's contents
+   */
+  virtual void print() const = 0;
+
+  /**
+   * Compares the given field with this field using the op specified in the predicate.
+   * Equivalent to field.compare() in the java implementation.
+   */
+  // if filter returns true, the tuple/attribute value needs to be included in the result
+  virtual bool filter(Predicate::opType op, Field* value) = 0;
+};
+}
