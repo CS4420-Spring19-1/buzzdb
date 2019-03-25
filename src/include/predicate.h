75341a537f707fb001171daa68b7a782081ed423
jonleeyz <jonleeyz@outlook.com>
2019-03-25 12:42:06 -0400

    [predicate.h] Refactor: Reorganize code chunks; add interface comments; update whitespace and indentation

--- src/include/predicate.h
+++ src/include/predicate.h
@@ -2,27 +2,55 @@
 
 #include <string>
 
-namespace emerald
-{
-    class Predicate {
-        public:
-            enum opType {
-                EQ,
-                NE,
-                GT,
-                LT,
-                GE,
-                LE
-            };
-            Predicate();
-            Predicate(std::string column_name, std::string op, std::string value);
-            std::string getColumn();
-            std::string getValue();
-            opType getOp();
-        private:
-            std::string column;
-            opType op;
-            std::string value;
-        
-    };
-} // emerald
+namespace emerald {
+/**
+ * Thie class is used to perform comparisons between Fields
+ */
+class Predicate {
+
+ private:
+  std::string column;
+  opType op;
+  std::string value;
+  
+  // not implemented: equality operator, copy constructor (necessary?)
+
+ public:
+ /**
+  * Constants used for return codes in Field::filter
+  */
+  enum opType {
+    EQ, // equals
+    NE, // not equals
+    GT, // greater than
+    LT, // less than
+    GE, // greater than or equal to
+    LE  // less than oe equal to
+  };
+
+  /**
+   * No argument constructor for the Predicate class
+   */
+  Predicate();
+
+  /**
+   * Constructor for the Predicate class
+   */
+  Predicate(std::string column_name, std::string op, std::string value);
+
+  /**
+   * Returns the predicate's column 
+   */
+  std::string getColumn();
+
+  /**
+   * Returns the predicate's value
+   */
+  std::string getValue();
+
+  /**
+   * Returns the predicate's op
+   */
+  opType getOp();
+};
+}
