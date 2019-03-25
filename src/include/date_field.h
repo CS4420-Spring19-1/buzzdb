127ba319d283fdc0200711c0d129728ace16a02f
jonleeyz <jonleeyz@outlook.com>
2019-03-25 12:32:53 -0400

    [date_field.h] Refactor: Reorganize code chunks; add interface comments; update whitespace and indentation

--- src/include/date_field.h
+++ src/include/date_field.h
@@ -4,16 +4,38 @@
 #include <ctime>
 #include <string>
 
-namespace emerald
-{
-    class DateField : public Field {
-        private:
-            std::time_t value;
-        public:
-            DateField(std::string v);
-            void print() const;
-            bool filter(Predicate::opType op, Field* value);
-            std::time_t getValue() const;
-            DateField(const DateField& field);
-    };
-} // emerald
+namespace emerald {
+class DateField : public Field {
+private:
+  std::time_t value;
+
+  // not implemented: equality operator, serialize, getType
+
+public:
+  /**
+   * Constructor for the DateField class
+   */
+  DateField(std::string v);
+
+  /**
+   * Copy constructor for the DateField class
+   */
+  DateField(const DateField &field);
+
+  /**
+   * Prints the date field's contents
+   */
+  void print() const override;
+
+  /**
+   * Compares the given field with this field using the op specified in the predicate.
+   * Equivalent to field.compare() in the java implementation.
+   */
+  bool filter(Predicate::opType op, Field *value) override;
+
+  /**
+   * Gets the date field's value
+   */
+  std::time_t getValue() const;
+};
+}
