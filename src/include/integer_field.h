c013fdf7f58d640c75a417d566d0f3ac53a94e3a
jonleeyz <jonleeyz@outlook.com>
2019-03-25 12:27:44 -0400

    [integer_field.h] Refactor: Reorganize code chunks; add interface comments; update whitespace and indentation

--- src/include/integer_field.h
+++ src/include/integer_field.h
@@ -2,16 +2,38 @@
 
 #include "field.h"
 
-namespace emerald
-{
-   class IntegerField : public Field{
-       private:
-            int value;
-        public:
-            IntegerField(int v);
-            void print() const;
-            bool filter(Predicate::opType op, Field* value);
-            int getValue() const;
-            IntegerField(const IntegerField& field);
-   }; 
-} // emerald
+namespace emerald {
+class IntegerField : public Field {
+ private:
+  int value;
+
+  // not implemented: equality operator, serialize, getType
+
+ public:
+  /**
+   * Constructor for the IntegerField class
+   */
+  IntegerField(int v);
+
+  /**
+   * Copy constructor for the IntegerField class
+   */
+  IntegerField(const IntegerField &field);
+
+  /**
+   * Prints the integer field's contents
+   */
+  void print() const override;
+
+  /**
+   * Compares the given field with this field using the op specified in the predicate.
+   * Equivalent to field.compare() in the java implementation.
+   */
+  bool filter(Predicate::opType op, Field *value) override;
+
+  /**
+   * Gets the integer field's value
+   */
+  int getValue() const;
+};
+}
