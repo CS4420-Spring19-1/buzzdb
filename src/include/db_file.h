b8e4e53f418ff778b2d1867610cf4ee2993b4bbc
jonleeyz <jonleeyz@outlook.com>
2019-03-28 13:39:54 -0400

    Create new interfaces: Page, PageId, DbFile, DbIterator, DbFileIterator

---
+++ src/include/db_file.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <vector>
+#include "page.h"
+
+namespace emerald {
+/**
+ * This is an interface for database files on disk. Each table is represented
+ * by a DbFile. DbFiles can fetch pages and traverse through tuples. Each file
+ * has a unique id used to store metadata about the table in the Catalog.
+ * DbFiles are generally accessed through the buffer pool, rather than directly
+ * by operators.
+ */
+class DbFile {
+ public:
+  /**
+   * Necessary for an interface class to ensure correct polymorphic deletion.
+   */
+  virtual ~DbFile();
+
+  /**
+   * Returns the unique id of the DbFile.
+   * This id can be used to identify this DbFile in the Catalog using the
+   * methods Catalog.get_db_file() and Catalog.get_tuple_desc().
+   * 
+   * Implementation note: This id will have to be generated somewhere.
+   * Ensure that each HeapFile has a "unique" id and that the same value is
+   * always returned for a particular HeapFile.
+   */
+  virtual int get_id() const = 0;
+
+  /**
+   * Returns the schema of the table stored in the DbFile
+   */
+  virtual TupleDesc get_tuple_desc() const = 0;
+
+  /**
+   * Reads the specified page from disk.
+   */
+  virtual Page ReadPage(PageId id) = 0;
+
+  /**
+   * Pushes the specified page to disk.
+   */
+  virtual void WritePage(Page p) = 0;
+
+  /**
+   * Adds the specified tuple to the DbFile on behalf of a transaction.
+   * This method will acquire a lock on the affected pages of the file, and may
+   * block until the lock can be acquired.
+   */
+  virtual std::vector<Page> AddTuple(TransactionId tid, Tuple t) = 0;
+
+  /**
+   * Removes the specified tuple from the DbFile on behalf of a transaction.
+   * This method will acquire a lock on the affected pages of the file, and may
+   * block until the lock can be acquired.
+   */
+  virtual Page DeleteTuple(TransactionId tid, Tuple t) = 0;
+
+  /**
+   * Returns an iterator over all tuples stored in this DbFile.
+   * The iterator must use BufferPool.getPage() to iterate through.
+   */
+  virtual DbFileIterator Iterator(TransactionId tid) = 0;
+};
+}
---
+++ src/include/db_iterator.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "tuple.h"
+
+namespace emerald {
+/**
+ * DbIterator is the iterator interface that all BuzzDB operators should
+ * implement. If the iterator is not open, none of the methods should work,
+ * and an IllegalStateException should be thrown. In addition to any resource
+ * allocation / deallocation, an open method should call any child iterator
+ * open methods, and in a close method, an iterator should call its children's
+ * close methods.
+ */
+class DbIterator {
+ public:
+  /**
+   * Opens the iterator. This must be called before any of the other methods.
+   * 
+   * Throws DBException when there are problems opening / closing the database.
+   */
+  virtual void Open() = 0;
+
+  /**
+   * Returns true if the iterator has more tuples.
+   * Throws IllegalStateException if the iterator has not been opened.
+   */
+  virtual bool HasNext() = 0;
+
+  /**
+   * Returns the next tuple from the operator.
+   * (typically implemented by reading from a child operator or an access
+   * method.)
+   * 
+   * Throws NoSuchElementException if there are no more tuples.
+   * Throws IllegalStateException if the iterator has not been opened.
+   */
+  virtual Tuple Next() = 0;
+
+  /**
+   * Resets the iterator to the start.
+   * 
+   * Throws DbException when rewind is unsupported.
+   * Throws IllegalStateException if the iterator has not been opened.
+   */
+  virtual void Rewind() = 0;
+
+  /**
+   * Returns the TupleDesc associated with the DbIterator.
+   */
+  virtual TupleDesc get_tuple_desc() const = 0;
+
+  /**
+   * Closes the DbIterator.
+   * 
+   * When the iterator is closed, calling Next(), HasNext() or Rewind() should
+   * fail by throwing an IllegalStateException.
+   */
+  virtual void Close() = 0;
+};
+}
