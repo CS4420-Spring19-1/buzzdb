b35af3d695b0d8bd847721cb0453fd8d8cd25d99
jonleeyz <jonleeyz@outlook.com>
2019-03-30 23:46:11 -0400

    Remove unused files

--- src/include/projection.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "table.h"
-#include "data_cube.h"
-#include "projection_expression.h"
-
-namespace emerald
-{
-    Table* ProjectFromDataCube(DataCube* datacube, std::vector<ProjectionExpression*> selected_columns);
-} // emerald
