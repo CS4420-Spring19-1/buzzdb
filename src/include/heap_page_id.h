557b416cc88b439a4b1e82abd47f7429defe466d
jonleeyz <jonleeyz@outlook.com>
2019-04-02 12:07:25 -0400

    [heap_page_id.h/cpp] Update class interface and implementation: HeapPageId

--- src/include/heap_page_id.h
+++ src/include/heap_page_id.h
@@ -1,26 +1,21 @@
 #pragma once
 
-#include "table_descriptor.h"
-#include "tuple.h"
-#include <unordered_map>
+#include "page_id.h"
 
 namespace emerald {
 class HeapPageId : public PageId {
  public:
+  HeapPageId();
 
   HeapPageId(int table_id, int page_number);
-
-  virtual ~HeapPageId();
 
   int get_table_id();
 
   int get_page_number();
 
-  int getHashCode();
+  virtual void Serialize(int rep[]);
 
   bool operator==(HeapPageId other);
-
-  std::vector<int> serialize();
 
  private:
   int table_id;
