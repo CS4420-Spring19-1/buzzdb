f58bfa0ebe16a6eb292f10bc1f9b73a5abd324f8
binzecai <binzecai@gatech.edu>
2019-04-08 11:27:31 +0800

    lockManager1

---
+++ src/database/lockManager.cpp
@@ -0,0 +1,56 @@
+#include "lockManager.h"
+
+namespace emerald {
+LockManager::LockManager(int num_pages) {
+  // mutexes = new Object[num_pages];
+  read_lock_holders = new std::vector<std::set<TransactionId*>>(num_pages);
+  write_lock_holders = new std::vector<TransactionId*>(num_pages);
+  for (size_t i = 0; i < num_pages; i++) {
+    // mutexes = new Object();
+    read_lock_holders.push_back(new std::set<TransactionId*>());
+    write_lock_holders.push_back(NULL);
+  }
+  writers = new std::vector<int>(num_pages);
+}
+
+LockManager::HoldsReadLock(TransactionId* tid, int idx) {
+  return read_lock_holders[idx].contains(tid);
+}
+
+LockManager::HoldsWriteLock(TransactionId* tid, int idx) {
+  return tid == write_lock_holders[idx];
+}
+
+bool LockManager::isHolding(TransactionId* tid, int idx) {
+  return HoldsWriteLock(tid, idx) || HoldsReadLock(tid, idx);
+}
+
+void LockManager::ArquireReadLock(TransactionId* tid, int idx) {
+
+}
+
+bool LockManager::ReleaseReadLock(TransactionId* tid, int idx) {
+
+}
+
+bool LockManager::HasOtherReader(TransactionId* tid, int idx) {
+
+}
+
+void LockManager::ArquireWriteLock(TransactionId* tid, int idx) {
+
+}
+
+void LockManager::ReleaseWriteLock(TransactionId* tid, int idx) {
+
+}
+
+void LockManager::Acquire(TransactionId* tid, int idx, Permissions* perm) {
+
+}
+
+bool LockManager::Release(TransactionId* tid, int idx) {
+  return ReleaseWriteLock(tid, idx) || ReleaseReadLock(tid, idx);
+}
+
+}
---
+++ src/include/lockManager.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <set>
+#include <vector>
+#include "transaction_id.h"
+
+namespace emerald {
+class LockManager {
+public:
+  LockManager(int num_pages);
+  isHolding(TransactionId* tid, int idx);
+  bool isHolding(TransactionId* tid, int idx);
+  void Acquire(TransactionId* tid, int idx, Permissions* perm);
+  bool Release(TransactionId* tid, int idx);
+
+private:
+  std::vector<std::set<TransactionId*>> read_lock_holders;
+  std::vector<TransactionId*> write_lock_holders;
+  std::vector<int> writers;
+  // private final Random rand = new Random();
+  int MIN_TIME = 100;
+  int MAX_TIME = 1000;
+
+  bool HoldsReadLock(TransactionId* tid, int idx);
+  
+  bool HoldWriteLock(TransactionId* tid, int idx); 
+  
+  void ArquireReadLock(TransactionId* tid, int idx);
+  
+  bool ReleaseReadLock(TransactionId* tid, int idx);
+  
+  bool HasOtherReader(TransactionId* tid, int idx);
+  
+  void ArquireWriteLock(TransactionId* tid, int idx);
+  
+  bool ReleaseWriteLock(TransactionId* tid, int idx);
+}
+} // emerald
