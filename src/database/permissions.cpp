543124b27eb462b2147f2a6cd2860c8b1424f6c0
jonleeyz <jonleeyz@outlook.com>
2019-04-12 13:50:58 -0400

    [permissions.h/cpp] Update implementation: Permissions

--- src/database/permissions.cpp
+++ src/database/permissions.cpp
@@ -1,10 +1,6 @@
 #include "permissions.h"
 
 namespace emerald {
-Permissions::Permissions(int perm_level) {
-  this->perm_level = perm_level;
+Permissions::Permissions(int perm_level) : perm_level(perm_level) {
 }
-
-Permissions::READ_ONLY = Permissions(1);
-Permissions::READ_WRITE = Permissions(0);
 }
