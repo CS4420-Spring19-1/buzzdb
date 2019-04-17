e0e4d61a45c85260d14db0fdd2b9ece368276fb4
binzecai <binzecai@gatech.edu>
2019-04-17 12:24:24 +0800

    utility

--- src/database/utility.cpp
+++ src/database/utility.cpp
@@ -1,7 +1,60 @@
+#include <stdio.h>
 #include "utility.h"
 
 namespace emerald {
-/**
- * To be ported over
- */
+
+
+static std::vector<Type*> Utility::GetTypes(int len) {
+  std::vector<Type*> types = new std::vector<Type*>(len);
+  for (int i = 0; i < len; i++) {
+    types[i] = Type::FieldType::INTEGER;
+  }
+  return types;
+}
+
+static std::vector<std::string> Utility::GetStrings(int len, std:::string val) {
+  std::vector<std::string> strings = new std::vector<std::string>(len);
+  for (int i = 0; i < len; i++) {
+    strings[i] = val + std::to_string(i);
+  }
+  return strings;
+}
+
+static TupleDesc* Utility::GetTupleDesc(int n, std::string name) {
+  return new TupleDesc(GetTypes(n), GetString(n, name));
+}
+
+
+static TupleDesc* Utility::GetTupleDesc(int n) {
+  return new TupleDesc(GetTypes(n));
+}
+
+static Tuple* Utility::GetHeapTuple(int n) {
+  Tuple* tup = new Tuple(GetTupleDesc(1));
+  tup->set_record_id(new RecordId(new HeapPageId(1, 2), 3));
+  tup->set_field(0, new IntegerField(n));
+  return tup;
+}
+
+static Tuple* Utility::GetHeapTuple(std::vector<int> tupdata) {
+  Tuple* tup = new Tuple(GetTupleDesc(tupdata.size()));
+  tup->set_record_id(new RecordId(new HeapPageId(1, 2), 3));
+  for (int i = 0; i < tupdata.size(); i++) {
+    tup->set_field(i, new IntegerField(tupdata[i]));
+  }
+  return tup;
+}
+
+static Tuple* Utility::GetTuple(std::vector<int> tupledata, int width) {
+  if (tupledata.size() != width) {
+    std::cout << "Get Hash Tuple has the wrong length!" << std::endl;
+    return nullptr;
+  }
+
+  Tuple* tup = new Tuple(GetTupleDesc(width));
+  for (int i = 0; i < width; i++) {
+    tup->set_field(i, new IntegerField(tupledata[i]));
+  }
+  return tup;
+}
 }
--- src/include/utility.h
+++ src/include/utility.h
@@ -1,7 +1,73 @@
 #pragma once
+#include <vector>
+#include <string>
+
+#include "integer_field.h"
+#include "record_id.h"
+#include "tuple.h"
+#include "tuple_desc.h"
+#include "heap_page_id.h"
 
 namespace emerald {
-/**
- * To be ported over
- */
+class Utility {
+public:
+  /*
+   * @return: Type array of length len populated with Type::INTEGER
+   */
+  static std::vector<Type*> GetTypes(int len);
+  /**
+   * @return a String array of length len populated with the (possibly null) strings in val,
+   * and an appended increasing integer at the end (val1, val2, etc.).
+   */
+  static std::vector<std::string> GetStrings(int len, std::string val);
+  
+  /**
+   * @return a TupleDesc with n fields of type Type.INT_TYPE, each named
+   * name + n (name1, name2, etc.).
+   */
+  static TupleDesc* GetTupleDesc(int n, std::string name);
+  
+  /**
+   * @return a TupleDesc with n fields of type Type.INT_TYPE
+   */
+  static TupleDesc* GetTupleDesc(int n);
+
+  /**
+   * @return a Tuple with a single IntField with value n and with
+   *   RecordId(HeapPageId(1,2), 3)
+   */
+  static Tuple* GetHeapTuple(int n);
+
+  /**
+   * @return a Tuple with an IntField for every element of tupdata
+   *   and RecordId(HeapPageId(1, 2), 3)
+   */
+  static Tuple* GetHeapTuple(std::vector<int> tupdata);
+
+  /**
+   * @return a Tuple with a 'width' IntFields with the value tupledata[i]
+   *         in each field.
+   *         do not set it's RecordId, hence do not distinguish which
+   *         sort of file it belongs to.
+   */
+  static Tuple* GetTuple(std::vector<int> tupledata, int width);
+
+  /**
+   * A utility method to create a new HeapFile with a single empty page,
+   * assuming the path does not already exist. If the path exists, the file
+   * will be overwritten. The new table will be added to the Catalog with
+   * the specified number of columns as IntFields.
+   */
+  static HeapFile* createEmptyHeapFile(std::string path, int cols);
+
+  /** Opens a HeapFile and adds it to the catalog.
+   *
+   * @param cols number of columns in the table.
+   * @param f location of the file storing the table.
+   * @return the opened table.
+   */
+  static HeapFile* OpenHeapFile(int cols, )
+private:
+
+}
 }
