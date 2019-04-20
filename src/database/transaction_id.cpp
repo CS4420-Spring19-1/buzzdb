f4c0f2478f168722191916337af90e7173650f87
jonleeyz <jonleeyz@outlook.com>
2019-04-20 04:19:53 -0400

    Overhaul class implementation: TransactionId

--- src/database/transaction_id.cpp
+++ src/database/transaction_id.cpp
@@ -1,28 +1,36 @@
 #include "transaction_id.h"
 
-// tests not written
-
 namespace buzzdb {
-/**
- * Initialization of private static member in TransactionId class
- */ 
-// std::atomic_long TransactionId::counter = 1;
-int TransactionId::counter = 1;
+std::atomic_long TransactionId::counter = {1};
 
-TransactionId::TransactionId() {
-  // id_value = counter.fetch_add(1);
-  id_value = counter++;
+TransactionId::TransactionId() : id_value(counter.fetch_add(1)) {
 }
 
-TransactionId::TransactionId(long id) {
-  this->id_value = id;
-}
-
-long TransactionId::get_id_value() {
+long TransactionId::get_id_value() const {
   return id_value;
 }
 
-bool TransactionId::operator==(TransactionId & other) {
+bool TransactionId::operator==(const TransactionId & other) {
   return id_value == other.id_value; 
+}
+
+bool TransactionId::operator!=(const TransactionId & other) {
+  return id_value != other.id_value; 
+}
+
+bool TransactionId::operator<(const TransactionId & other) {
+  return id_value > other.id_value; 
+}
+
+bool TransactionId::operator>(const TransactionId & other) {
+  return id_value > other.id_value; 
+}
+
+bool TransactionId::operator<=(const TransactionId & other) {
+  return id_value <= other.id_value; 
+}
+
+bool TransactionId::operator>=(const TransactionId & other) {
+  return id_value >= other.id_value; 
 }
 }
