f3db83c7f0967521537108da5f6fcec14085b7b4
jonleeyz <jonleeyz@outlook.com>
2019-04-20 19:44:52 -0400

    Update interface documentation: DbFile, DbFileIterator, DbIterator

--- src/include/db_iterator.h
+++ src/include/db_iterator.h
@@ -4,12 +4,13 @@
 
 namespace buzzdb {
 /**
- * DbIterator is the iterator interface that all BuzzDB operators should
- * implement. If the iterator is not open, none of the methods should work,
- * and an IllegalStateException should be thrown. In addition to any resource
- * allocation / deallocation, an open method should call any child iterator
- * open methods, and in a close method, an iterator should call its children's
- * close methods.
+ * The DbIterator interface class is an iterator interface for BuzzDb
+ * operators.
+ * - If the iterator is not open, none of the methods should work, and an
+ *   IllegalStateException should be thrown. 
+ * - In addition to any resource allocation / deallocation, an open method
+ *   should call any child iterator open methods, and in a close method, an
+ *   iterator should call its children's close methods.
  */
 class DbIterator {
  public:
@@ -21,43 +22,47 @@ class DbIterator {
   virtual ~DbIterator();
 
   /**
-   * Opens the iterator. This must be called before any of the other methods.
+   * Opens the iterator.
+   * This must be called before any of the other methods are used.
    * 
-   * Throws DBException when there are problems opening / closing the database.
+   * Throws:
+   * - DBException: When there are problems opening / closing the database.
    */
   virtual void Open() = 0;
 
   /**
    * Returns true if the iterator has more tuples.
-   * Throws IllegalStateException if the iterator has not been opened.
+   * 
+   * Throws:
+   * - IllegalStateException: If the iterator is not open.
    */
   virtual bool HasNext() = 0;
 
   /**
    * Returns the next tuple from the operator.
-   * (typically implemented by reading from a child operator or an access
-   * method.)
    * 
-   * Throws NoSuchElementException if there are no more tuples.
-   * Throws IllegalStateException if the iterator has not been opened.
+   * Throws:
+   * - IllegalStateException: If the iterator is not open.
+   * - NoSuchElementException: If there are no more tuples.
    */
-  virtual Tuple Next() = 0;
+  virtual Tuple & Next() = 0;
 
   /**
    * Resets the iterator to the start.
    * 
-   * Throws DbException when rewind is unsupported.
-   * Throws IllegalStateException if the iterator has not been opened.
+   * Throws:
+   * - IllegalStateException: If the iterator is not open.
+   * - DbException: When rewind is unsupported.
    */
   virtual void Rewind() = 0;
 
   /**
-   * Returns the TupleDesc associated with the DbIterator.
+   * Returns the TupleDesc associated with the current tuple.
    */
-  virtual TupleDesc get_tuple_desc() const = 0;
+  virtual const TupleDesc & get_tuple_desc() const = 0;
 
   /**
-   * Closes the DbIterator.
+   * Closes the iterator.
    * 
    * When the iterator is closed, calling Next(), HasNext() or Rewind() should
    * fail by throwing an IllegalStateException.
