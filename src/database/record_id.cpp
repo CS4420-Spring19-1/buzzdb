c505fab3bba85168eece2f480960f8a967225006
jonleeyz <jonleeyz@outlook.com>
2019-04-21 15:38:58 -0400

    Update class implementation: RecordId

--- src/database/record_id.cpp
+++ src/database/record_id.cpp
@@ -2,29 +2,23 @@
 #include "heap_page_id.h"
 
 namespace buzzdb {
-RecordId::RecordId(HeapPageId * pid, int tuple_number) {
-  this->pid = pid;
-  this->tuple_number = tuple_number;
+RecordId::RecordId(PageId & pid, int tuple_number)
+    : pid(pid), tuple_number(tuple_number) {
 }
 
 int RecordId::get_tuple_number() const {
   return tuple_number;
 }
 
-HeapPageId* RecordId::get_page_id() const {
+const PageId & RecordId::get_page_id() const {
   return pid;
 }
 
-bool RecordId::operator==(RecordId other) {
-  return this->pid->get_page_number() == other.pid->get_page_number()
-      && this->tuple_number == other.get_tuple_number()
-      && this->pid->get_table_id() == other.pid->get_table_id();
+bool RecordId::operator==(const RecordId & other) {
+  return pid == other.pid && tuple_number == other.tuple_number;
 }
 
-// bool RecordId::Equal(RecordId* other) {
-//   return this->pid->get_page_number() == other->get_page_id()->get_page_number()
-//       && this->tuple_number == other->get_tuple_number() 
-//       && this->pid->get_table_id() == other->get_page_id()->get_table_id();
-// }
-
+bool RecordId::operator!=(const RecordId & other) {
+  return (*this == other);
+}
 }
