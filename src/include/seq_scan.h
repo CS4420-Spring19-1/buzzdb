5e9a6b0087bb379bd1bda7ac6dcdd7d68599c20f
jonleeyz <jonleeyz@outlook.com>
2019-04-20 20:13:04 -0400

    Tweak class implementation: SeqScan

--- src/include/seq_scan.h
+++ src/include/seq_scan.h
@@ -3,49 +3,88 @@
 #include <string>
 #include "db_file.h"
 #include "db_file_iterator.h"
+#include "db_iterator.h"
 #include "transaction_id.h"
 
 namespace buzzdb {
-class SeqScan {
+/**
+ * The SeqSca class is an implementation of a sequential scan access method
+ * of a table.
+ * - The table is represented by a DbFile and each tuple in the DbFile is read.
+ * - There is no constraint on the order tuples are to be read.
+ */
+class SeqScan : DbIterator {
  public:
   /**
-    * Creates a sequential scan over the specified table as a part of the
-    * specified transaction.
-    * 
-    * @param tid
-    *            The transaction this scan is running as a part of.
-    * @param tableid
-    *            the table to scan.
-    * @param tableAlias
-    *            the alias of this table (needed by the parser); the returned
-    *            tupleDesc should have fields with name tableAlias.fieldName
-    *            (note: this class is not responsible for handling a case where
-    *            tableAlias or fieldName are null. It shouldn't crash if they
-    *            are, but the resulting name can be null.fieldName,
-    *            tableAlias.null, or null.null).
-    */
+   * Constructor.
+   * Creates a sequential scan over the specified table as part of the
+   * specified transaction.
+   * 
+   * Parameters:
+   * - tid: The transaction the scan is running as a part of.
+   * - tableid: The table to scan.
+   * - tableAlias: The alias of the table.
+   */
   SeqScan(TransactionId & tid, int table_id, std::string table_alias);
 
-  void Open();
+  /**
+   * Destructor.
+   */
+  ~SeqScan() = default;
 
   /**
-    * Returns the TupleDesc with field names from the underlying HeapFile,
-    * prefixed with the tableAlias string from the constructor. This prefix
-    * becomes useful when joining tables containing a field(s) with the same
-    * name.
-    * 
-    * @return the TupleDesc with field names from the underlying HeapFile,
-    *         prefixed with the tableAlias string from the constructor.
+   * Opens the iterator.
+   * This must be called before any of the other methods are used.
+   * 
+   * Throws:
+   * - DBException: When there are problems opening / closing the database.
+   */
+  void Open() override;
+
+  /**
+   * Returns true if the iterator has more tuples.
+   * 
+   * Throws:
+   * - IllegalStateException: If the iterator is not open.
+   */
+  bool HasNext() override;
+
+  /**
+   * Returns the next tuple from the operator.
+   * 
+   * Throws:
+   * - IllegalStateException: If the iterator is not open.
+   * - NoSuchElementException: If there are no more tuples.
+   */
+  Tuple & Next() override;
+
+  /**
+   * Resets the iterator to the start.
+   * 
+   * Throws:
+   * - IllegalStateException: If the iterator is not open.
+   * - DbException: When rewind is unsupported.
+   */
+  void Rewind() override;
+
+  /**
+   * Returns the TupleDesc associated with the current tuple.
+   * 
+   * // Not done
+   * Returns the TupleDesc with field names from the underlying HeapFile,
+   * prefixed with the tableAlias string from the constructor. This prefix
+   * becomes useful when joining tables containing a field(s) with the same
+   * name.
     */
-  TupleDesc get_tuple_desc() const;
+  const TupleDesc & get_tuple_desc() const override;
 
-  bool HasNext();
-
-  Tuple *Next();
-
-  void Close();
-
-  void Rewind();
+  /**
+   * Closes the iterator.
+   * 
+   * When the iterator is closed, calling Next(), HasNext() or Rewind() should
+   * fail by throwing an IllegalStateException.
+   */
+  void Close() override;
 
  private:
   DbFile * db_file;
