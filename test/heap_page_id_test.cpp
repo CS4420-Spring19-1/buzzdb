bf7dd49b4b88c5ba46d2d79c4640c75539f7927f
jonleeyz <jonleeyz@outlook.com>
2019-04-21 02:41:53 -0400

    Update test suite implementation: HeapPageIdTest

--- test/heap_page_id_test.cpp
+++ test/heap_page_id_test.cpp
@@ -2,68 +2,105 @@
 #include "heap_page_id.h"
 
 namespace buzzdb {
-class PidTests {
- public:
-  PidTests() {
-    pid = new HeapPageId(1, 1);
-  }
+/**
+ * Unit test suite: HeapPageId
+ */
+TEST(GetterTests, GetTableIdWorksCorrectly) {
+  HeapPageId heap_page_id(1, 1);
 
-  ~PidTests() {
-    delete pid;
-  }
+  EXPECT_EQ(1, heap_page_id.get_table_id());
+}
 
-  void test1() {
-    ASSERT_EQ(1, pid->get_table_id());
-  }
+TEST(GetterTests, GetPageNumberWorksCorrectly) {
+  HeapPageId heap_page_id(1, 1);
 
-  void test2() {
-    ASSERT_EQ(1, pid->get_page_number());
-  }
+  EXPECT_EQ(1, heap_page_id.get_page_number());
+}
 
-  void test3() {
-    HeapPageId pid1(1, 1);
-    HeapPageId pid1Copy(1, 1);
-    HeapPageId pid2(2, 2);
+TEST(ConstructorTests, ConstructorWorksCorrectly) {
+  HeapPageId heap_page_id_new(22, 0);
 
-    // equals() with wrong type should return false
+  EXPECT_EQ(22, heap_page_id_new.get_table_id());
+  EXPECT_EQ(0, heap_page_id_new.get_page_number());
+}
 
-    ASSERT_TRUE(pid1 == pid1);
-    ASSERT_TRUE(pid1 == pid1Copy);
-    ASSERT_TRUE(pid1Copy == pid1);
-    ASSERT_TRUE(pid2 == pid2);
+TEST(ConstructorTests, CopyConstructorWorksCorrectly) {
+  HeapPageId heap_page_id(1, 1);
+  HeapPageId heap_page_id_copy = heap_page_id;
 
-    ASSERT_FALSE(pid1 == pid2);
-    ASSERT_FALSE(pid1Copy == pid2);
-    ASSERT_FALSE(pid2 == pid1);
-    ASSERT_FALSE(pid1Copy == pid2);
+  EXPECT_EQ(heap_page_id.get_table_id(), heap_page_id_copy.get_table_id());
+  EXPECT_EQ(heap_page_id.get_page_number(), heap_page_id_copy.get_page_number());
 
-  }
- private:
-  HeapPageId* pid;
-};
+  // check that the two objects are distinct
+  EXPECT_NE(&heap_page_id, &heap_page_id_copy);
+}
 
-PidTests* pid_test;
-// Unit test for HeapPageId->getTableId()
-TEST(PidTest, GetTableId) {
-  pid_test = new PidTests();
-  pid_test->test1();
-};
+TEST(OperatorOverloadTests, EqualityOperatorWorksCorrectly) {
+  HeapPageId heap_page_id(1, 1);
+  HeapPageId heap_page_id_same(1, 1);
+  HeapPageId heap_page_id_different_table_id(10, 1);
+  HeapPageId heap_page_id_different_page_number(1, 27);
+  HeapPageId heap_page_id_different(10, 27);
+  HeapPageId heap_page_id_copy = heap_page_id;
 
-// Unit test for HeapPageId->pageno()
-TEST(PidTest, GetTableNumber) {
-  pid_test->test2();
-};
-// Unit test for HeapPageId.hashCode()
-// Not exist in c++
-TEST(PidTest, Equal) {
-  pid_test->test3();
-  delete pid_test;
+  // check comparison with self
+  EXPECT_TRUE(heap_page_id == heap_page_id);
+
+  // check comparison with same values
+  EXPECT_TRUE(heap_page_id == heap_page_id_same);
+  
+  // check commutativity of operator
+  EXPECT_TRUE(heap_page_id_same == heap_page_id);
+
+  // check comparison with copy of self 
+  EXPECT_TRUE(heap_page_id == heap_page_id_copy);
+
+  // check comparison with different table id
+  EXPECT_FALSE(heap_page_id == heap_page_id_different_table_id);
+
+  // check comparison with different page number
+  EXPECT_FALSE(heap_page_id == heap_page_id_different_page_number);
+
+  // check comparison with different values
+  EXPECT_FALSE(heap_page_id == heap_page_id_different);
+}
+
+TEST(OperatorOverloadTest, InequalityOperatorWorksCorrectly) {
+  HeapPageId heap_page_id(1, 1);
+  HeapPageId heap_page_id_same(1, 1);
+  HeapPageId heap_page_id_different_table_id(10, 1);
+  HeapPageId heap_page_id_different_page_number(1, 27);
+  HeapPageId heap_page_id_different(10, 27);
+  HeapPageId heap_page_id_copy = heap_page_id;
+
+  // check comparison with self
+  EXPECT_FALSE(heap_page_id != heap_page_id);
+
+  // check comparison with same values
+  EXPECT_FALSE(heap_page_id != heap_page_id_same);
+  
+  // check commutativity of operator
+  EXPECT_FALSE(heap_page_id_same != heap_page_id);
+
+  // check comparison with copy of self 
+  EXPECT_FALSE(heap_page_id != heap_page_id_copy);
+
+  // check comparison with different table id
+  EXPECT_TRUE(heap_page_id != heap_page_id_different_table_id);
+
+  // check comparison with different page number
+  EXPECT_TRUE(heap_page_id != heap_page_id_different_page_number);
+
+  // check comparison with different values
+  EXPECT_TRUE(heap_page_id != heap_page_id_different);
 }
 }
 
+/**
+ * Test driver
+ */
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     int result = RUN_ALL_TESTS();
     return result;
-    
 }
